2a3d2457eb2d7be7c30bed8bb15bfb1c991808b2
swj <shinwj4664@gmail.com>
2023-05-27 11:25:20 +0900

    component data send

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
-import axios from 'axios';
 import Search from '../src/components/Search';
 import KakaoMap from '../src/components/KakaoMap';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import { useState } from 'react';
+
+let libraryData = [];
+
 function App() {
+  const receiveData = (data) => {
+    libraryData = data
+  }
+  
+  let use = useNavigate();
   return (
     <>
-      <a href=""><Search /></a>
-      <KakaoMap />
+      <Routes>
+        <Route path='/' element={
+          <Link to="/detail"><Search receiveData={receiveData}/></Link>
+        }>
+        </Route>
+        <Route path='/detail' element={<div>
+          <Detail libraryData={libraryData}/>
+          <KakaoMap/>
+        </div>}></Route>
+      </Routes>
     </>
   );
+}
+
+function Detail(props) {
+  console.log(props.libraryData[0].lbrryNm)
+  return(
+    <>
+    도서관내용
+    <h4>{props}</h4>
+    </>
+  )
 }
 
 
