ea79e8c0523cb08a7344d86cfd4554e3f8bf3630
swj <shinwj4664@gmail.com>
2023-05-27 23:16:44 +0900

    paging

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
 import './App.css';
-import Search from '../src/components/Search';
-import KakaoMap from '../src/components/KakaoMap';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-import { useState } from 'react';
+import {SearchFetch,Detail} from '../src/components/Search';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-let libraryData = [];
 
 function App() {
-  const receiveData = (data) => {
-    libraryData = data
-  }
-  
-  let use = useNavigate();
   return (
-    <>
       <Routes>
-        <Route path='/' element={
-          <Link to="/detail"><Search receiveData={receiveData}/></Link>
-        }>
-        </Route>
-        <Route path='/detail' element={<div>
-          <Detail libraryData={libraryData}/>
-          <KakaoMap/>
-        </div>}></Route>
+        <Route path="/" element={<SearchFetch />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
-    </>
   );
 }
-
-function Detail(props) {
-  console.log(props.libraryData[0].lbrryNm)
-  return(
-    <>
-    도서관내용
-    <h4>{props}</h4>
-    </>
-  )
-}
-
 
 
 export default App;
