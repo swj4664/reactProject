b3b34a1fb614a6fc866648a38a1c5657d2cf70e0
swj <shinwj4664@gmail.com>
2023-05-27 23:16:44 +0900

    paging

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,19 +1,47 @@
 import { useEffect, useState } from 'react';
+import '../css/KakaoMap.css'
 const { kakao } = window;
 
-function Kakao(){
-    useEffect(()=>{
+function Kakao(props) {
+    useEffect(() => {
         const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         const options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            center: new kakao.maps.LatLng(props.latitude, props.longitude), //지도의 중심좌표.
             level: 3 //지도의 레벨(확대, 축소 정도)
         };
-        
-        const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    },[])
 
-    return(
-        <div id="map" style={{width:500 +'px', height:400 + 'px'}}></div>
+
+        const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        //마커가 표시 될 위치
+        let markerPosition = new kakao.maps.LatLng(
+            props.latitude,
+            props.longitude
+        );
+
+        // 마커를 생성
+        let marker = new kakao.maps.Marker({
+            position: markerPosition,
+        });
+
+        // 마커를 지도 위에 표시
+        marker.setMap(map);
+
+        let iwContent = `<div class="info-title">${props.name} <br><a href="https://map.kakao.com/link/to/${props.name},${props.latitude},${props.longitude}" style="color:blue" target="_blank">길찾기</a></div>` // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
+        // 인포윈도우를 생성합니다
+        let infowindow = new kakao.maps.InfoWindow({
+            content: iwContent
+        });
+
+        infowindow.open(map, marker);
+
+        document.querySelector('.info-title').parentElement.style.border='0px'
+        document.querySelector('.info-title').style.background='black'
+
+    }, [])
+
+    return (
+        <div id="map" style={{ width: 500 + 'px', height: 400 + 'px' }}></div>
     )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,49 +1,114 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import KakaoMap from './KakaoMap';
+import { Link, useParams, useLocation } from 'react-router-dom'
+
+function SearchFetch() {
+  const [data, setData] = useState({ items: [] });
+  const [query, setQuery] = useState("포항시");
+
+  // 페이징관련 변수
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; // 한 페이지 리스트 수
+  const startItem = (currentPage - 1) * itemsPerPage; // 보여주기 시작할 리스트
+  const endItem = startItem + itemsPerPage;
+  const currentPageData = data.items.slice(startItem, endItem) // 현재 페이지에 보여지는 리스트
+  const pageCount = Math.ceil(data.items.length / itemsPerPage)
 
 
-function SearchFetch({receiveData}){
-    const [data, setData] = useState({ items: [] });
-    const [query, setQuery] = useState("포항시");
 
-    //useEffect는 비동기적으로 동작
-    useEffect(() => {
-      let completed = false; //초기에는 실행해야 되기때문에 false flag 변수
+  //useEffect는 비동기적으로 동작
+  useEffect(() => {
+    let completed = false; //초기에는 실행해야 되기때문에 false flag 변수
 
-      //query를 리턴하는 함수를 result에 할당
-      async function get() {
-        try {
+    //query를 리턴하는 함수를 result에 할당
+    async function get() {
+      try {
         const result = await axios.get(
-            `http://api.data.go.kr/openapi/tn_pubr_public_lbrry_api?serviceKey=efSREml9J5VV3VB7rUN3F1QrndFlWhJWdlTOrfPyf5al8SixYeyO%2FhQJIk9GdSCIHklk5t6iomUE6jj9uRTkZA%3D%3D&pageNo=1&numOfRows=100&type=json&SIGNGU_NM=${query}`
+          `http://api.data.go.kr/openapi/tn_pubr_public_lbrry_api?serviceKey=efSREml9J5VV3VB7rUN3F1QrndFlWhJWdlTOrfPyf5al8SixYeyO%2FhQJIk9GdSCIHklk5t6iomUE6jj9uRTkZA%3D%3D&pageNo=1&numOfRows=100&type=json&SIGNGU_NM=${query}`
         );
-        if (!completed){
-            console.log(result.data.response.body.items);
-            setData(result.data.response.body || {items : []});
-            receiveData(result.data.response.body.items)
-        }else{
-            console.log(completed);
+        if (!completed) {
+          // console.log(result.data.response.body.items);
+          setData(result.data.response.body || { items: [] });
+        } else {
+          // console.log(completed);
         }
-        } catch(error){
-          console.log(error);
-        }
+      } catch (error) {
+        console.log(error);
       }
-      get();
+    }
+    get();
 
-      return () => {
-        completed = true;
-      };
-      //query가 변할때 useEffect를 실행해야하는 시점이다
-    }, [query]); //input에 값이 변경이 되었을때 effect를 실행한다
-    return (
-      <>
-        <input value={query} onChange={(e) => setQuery(e.target.value)} />
-        <ul>
-          {data.items &&
-           data.items.map((value,index) => (
-            <a href={index}><li key={index}>{value.lbrryNm} : {value.closeDay}</li></a>
-           ))}
-        </ul>
-      </>
-    );
+    return () => {
+      completed = true;
+    };
+    //query가 변할때 useEffect를 실행해야하는 시점이다
+  }, [query]); //input에 값이 변경이 되었을때 effect를 실행한다
+  function pageBtn(){
+    const result = [];
+    for(let i = 0; i < pageCount; i++){
+      
+      result.push(<button key={i} onClick={()=> setCurrentPage(i+1)}>{i+1}</button>)
+    }
+    return result
+  }
+
+  return (
+    <>
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <ul>
+        {currentPageData &&
+          currentPageData.map((value, index) => (
+            <Link to={`/detail/${index}?name=${value.lbrryNm}&closeDay=${value.closeDay}&longitude=${value.longitude}&latitude=${value.latitude}`} key={index}>
+              <li>{value.lbrryNm} : {value.closeDay}</li>
+            </Link>
+          ))}
+      </ul>
+
+      
+      <button onClick={()=>setCurrentPage(currentPage-1)} disabled={currentPage===1}>
+        이전 페이지
+      </button>
+      {pageBtn()}
+      <button onClick={()=>setCurrentPage(currentPage+1)} disabled={currentPage===pageCount}>
+        다음 페이지
+      </button>
+    </>
+  );
+
+ 
+
 }
-export default SearchFetch;
+/**
+ * @see SearchFetch() //참조해서 봐야할것
+ */
+function Detail() {
+
+  /**
+   * id : 인덱스번호
+   * location : 현재URL가져오기
+   * searchParams : 쿼리스트링 값 가져오기
+   * name: 도서관이름
+   * closeDay : 휴관일
+   * @returns 도서관이름,휴관일,경도,위도,카카오맵
+   */
+  const { id } = useParams();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const name = searchParams.get('name');
+  const closeDay = searchParams.get('closeDay');
+  const longitude = searchParams.get('longitude');
+  const latitude = searchParams.get('latitude');
+
+  return (
+    <>
+      도서관 내용
+      <p>도서관 이름: {name}</p>
+      <p>휴관일: {closeDay}</p>
+      <p>경도 : {longitude}</p>
+      <p>위도 : {latitude}</p>
+      <KakaoMap index={id} longitude={longitude} latitude={latitude} name={name} />
+    </>
+  );
+}
+export { SearchFetch, Detail };
